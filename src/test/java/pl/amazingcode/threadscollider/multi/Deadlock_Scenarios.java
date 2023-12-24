b77e0496a2b28f88da69e38033677d64a6f55b86
Piotr Stawirej <piotr.stawirej@gmail.com>
2023-12-24 13:29:06 +0100

    style: formatting

--- src/test/java/pl/amazingcode/threadscollider/multi/Deadlock_Scenarios.java
+++ src/test/java/pl/amazingcode/threadscollider/multi/Deadlock_Scenarios.java
@@ -3,11 +3,6 @@ package pl.amazingcode.threadscollider.multi;
 import static org.assertj.core.api.BDDAssertions.then;
 import static pl.amazingcode.threadscollider.multi.MultiThreadsCollider.MultiThreadsColliderBuilder.multiThreadsCollider;
 
-import java.lang.management.LockInfo;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MonitorInfo;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -18,33 +13,6 @@ import org.junit.jupiter.api.RepeatedTest;
 final class Deadlock_Scenarios {
 
   private static final int ACTION_THREADS_COUNT = Runtime.getRuntime().availableProcessors() / 2;
-
-  private static String dump;
-
-  private static String threadDump(boolean lockedMonitors, boolean lockedSynchronizers) {
-    StringBuffer threadDump = new StringBuffer(System.lineSeparator());
-    ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-    for (long threadId : threadMXBean.findDeadlockedThreads()) {
-      ThreadInfo threadInfo = threadMXBean.getThreadInfo(threadId);
-      if (threadInfo != null) {
-        threadDump.append(threadInfo);
-        threadDump.append(System.lineSeparator());
-        if (lockedMonitors) {
-          for (MonitorInfo monitorInfo : threadInfo.getLockedMonitors()) {
-            threadDump.append(monitorInfo.toString());
-            threadDump.append(System.lineSeparator());
-          }
-        }
-        if (lockedSynchronizers) {
-          for (LockInfo lockInfo : threadInfo.getLockedSynchronizers()) {
-            threadDump.append(lockInfo.toString());
-            threadDump.append(System.lineSeparator());
-          }
-        }
-      }
-    }
-    return threadDump.toString();
-  }
 
   @RepeatedTest(1)
   void Detect_deadlock() {
@@ -73,19 +41,6 @@ final class Deadlock_Scenarios {
           }
         };
 
-    //    ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-    //    executor.schedule(
-    //        () -> {
-    //          dump = threadDump(true, true);
-    //          System.out.println(dump);
-    //        },
-    //        1,
-    //        TimeUnit.SECONDS);
-
-    // When
-    //    assertTimeoutPreemptively(
-    //        Duration.ofSeconds(3),
-    //        () -> {
     try (MultiThreadsCollider collider =
         multiThreadsCollider()
             .withAction(r1)
@@ -99,12 +54,8 @@ final class Deadlock_Scenarios {
 
       collider.collide();
     }
-    //        });
 
     // Then
-    exceptions.forEach(e -> System.out.println(e.toString()));
-    //    then(list1).hasSize(ACTION_THREADS_COUNT);
-    //    then(list2).hasSize(ACTION_THREADS_COUNT);
     then(exceptions).isEmpty();
   }
 }
